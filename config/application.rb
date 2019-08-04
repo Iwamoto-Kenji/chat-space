159f0f251c250cb2d097490bfda40fad5685e8ca
Iwamoto-Kenji <08030531kk>
2019-08-04 17:45:47 +0900

    MakeMessageSend

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module ChatSpace
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -19,5 +21,9 @@ module ChatSpace
       g.test_framework false
     end
     config.i18n.default_locale = :ja
+
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance| 
+      html_tag
+    end
   end
 end
