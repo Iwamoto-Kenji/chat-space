cfb992ccae32a657daea1238f94a2690b594cca3
Iwamoto-Kenji <08030531kk>
2019-08-08 16:55:47 +0900

    非同期通信

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -34,7 +34,6 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messageslists').append(html)
-      $('.input-box').reset;
       $('.messageslists').animate({scrollTop: $('.messageslists')[0].scrollHeight}, 'fast');
       $('#new_message')[0].reset();
     })
