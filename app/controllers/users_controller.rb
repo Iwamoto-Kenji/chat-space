9438facfae60f12fc9ce760b41cb1aa605b9bc18
Iwamoto-Kenji <08030531kk>
2019-08-09 21:55:46 +0900

    インクリメンタルサーチ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,29 @@
-# class UsersController < ApplicationController
-#   def index
-#     if params[:keyword] != ""
-#     @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
-#     end
+class UsersController < ApplicationController
+  def index
+    if params[:keyword] != ""
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    end
 
-#     respond_to do |format|
-#       format.html
-#       format.json
-#     end
-#   end
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
 
-#   def edit
-#   end
+  def edit
+  end
 
-#   def update
-#     if current_user.update(user_params)
-#       redirect_to root_path
-#     else
-#       render :edit
-#     end
-#   end
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
-#   private
+  private
 
-#   def user_params
-#     params.require(:user).permit(:name, :email)
-#   end
-# end
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
